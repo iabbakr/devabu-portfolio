fba8d40b3dce75fcf029a488f6ae841ad6802ce0
iabbakr <abubakaribrahim603@gmail.com>
2025-05-14 20:36:23 +0100

    added resume and links

--- src/app/data.js
+++ src/app/data.js
@@ -96,19 +96,19 @@ export const BtnList = [
   { label: "Contact", link: "/contact", icon: "contact", newTab: false },
   {
     label: "Github",
-    link: "https://www.github.com/codebucks27",
+    link: "https://github.com/iabbakr",
     icon: "github",
     newTab: true,
   },
   {
     label: "LinkedIn",
-    link: "https://www.linkedin.com/in/codebucks",
+    link: "https://www.linkedin.com/in/abubakar-ibrahim-a037b4139/",
     icon: "linkedin",
     newTab: true,
   },
   {
     label: "X",
-    link: "https://www.x.com/code_bucks",
+    link: "https://x.com/iabbakr",
     icon: "twitter",
     newTab: true,
   },
